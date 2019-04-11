f2f89358dec31196ebcd72fc642745c092fea337
Kasia Bondarava <kasia.bondarava@gmail.com>
2019-04-11 13:16:26 -0700

    Added conditional className change depend on todo.completed value, changed styling accordantly with the className

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -4,8 +4,8 @@ const Todo = props => {
     return(
         <div className='todo'>
             <div className='todo-content'>
-                <h4 className='todo-bullet'>※</h4>
-                <h4 className='todo-text' onClick={props.onClick} style={props.todo.completed ? {textDecoration: 'line-through', color: 'rgb(44, 44, 44)'} : {textDecoration: 'none', color: 'white'} }>{props.todo.value}</h4>
+                <h4 className={props.todo.completed ? 'todo-bullet-completed' : 'todo-bullet'}>※</h4>
+                <h4 className={props.todo.completed ? 'todo-text-completed' : 'todo-text'} onClick={props.onClick}>{props.todo.value}</h4>
             </div>
             <p>✘</p>
         </div>
